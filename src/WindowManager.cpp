09f12c4376a8c23d5eda476adb48da9349e8a785
luca-d <lucapiai98@proton.me>
2023-09-18 00:27:59 +0200

    fix: dichiarazione variabili statiche

--- src/WindowManager.cpp
+++ src/WindowManager.cpp
@@ -7,19 +7,4 @@ std::shared_ptr<WindowManager> WindowManager::instance_ = nullptr;
 std::map<std::string, std::shared_ptr<Window>> WindowManager::windows_;
 std::shared_ptr<Window> WindowManager::focused_;
 
-std::shared_ptr<Window> WindowManager::create_win(const std::string& name, int h, int l, int y, int x) {
-    focused_ = std::make_shared<Window>(h, l, y, x);
-    windows_[name] = focused_;
-    return focused_;
-}
-
-bool WindowManager::find_win(const std::string& name) {
-    auto ptr = windows_.find(name);
-    if (ptr != windows_.end()) {
-        focused_ = ptr->second;
-        return true;
-    }
-    return false;
-}
-
 } // end namespace
